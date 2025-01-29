9f50aadb902c81a3c98c18b7394225716ee888b3
Gaurav Parker~ <gauravparker955@gmail.com>
2025-01-30 00:20:26 +0530

    hwllo

---
+++ test.js
@@ -0,0 +1,7 @@
+const accid=13223
+let accountpassword ="2313442"
+var accemail="gauravparker955@gmail.com"
+let acccity="ponda"
+console.table([acccity,accemail,accid,accountpassword])
+/* var is outdated 
+prefered not to used */
